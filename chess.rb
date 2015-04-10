cb368154d8172ace990ec484983a40351771389a
fontcuberta <isafontcu@gmail.com>
2015-04-10 13:35:54 +0200

    Horse, Pawn created

--- chess.rb
+++ chess.rb
@@ -1,10 +1,10 @@
 require 'pry'
 $LOAD_PATH << '.'
 require 'chess_mod'
-#require 'chess_mod'
 
 
 
+#Piece class just initialize the common atts for every piece in the set
 class Piece
 	attr_accessor :name
 	attr_accessor :color
@@ -15,9 +15,10 @@ class Piece
 	end
 end
 
-class Rook < Piece
+#King class has a validate_movement method. He could be moved one step in any direction
+class King < Piece
 	def validate_movement(*to_go)
-		if  Direction.straight(to_go, @actual_position)
+		if ((Direction.diagonal(to_go, @actual_position) or Direction.straight(to_go, @actual_position)) and Direction.one_step(to_go, @actual_position))
 			@actual_position = Asignation.new_position(to_go, @actual_position, @name, @color)
 		else
 			Asignation.invalid_movement
@@ -25,16 +26,7 @@ class Rook < Piece
 	end
 end
 
-class Bishop < Piece
-	def validate_movement(*to_go)
-		if Direction.diagonal(to_go, @actual_position)
-			@actual_position = Asignation.new_position(to_go, @actual_position, @name, @color)
-		else
-			Asignation.invalid_movement
-		end
-	end
-end
-
+#Queen class has a validate_movement method. She could be moved in a straigth or diagonal direction, as many steps as desired 
 class Queen < Piece
 	def validate_movement(*to_go)
 		if Direction.diagonal(to_go, @actual_position) or Direction.straight(to_go, @actual_position)
@@ -46,9 +38,43 @@ class Queen < Piece
 	end
 end
 
-class King < Piece
+#Rook classd defines a validate_movement method. The rook could be moved in any number of steps but only in straight direction
+class Rook < Piece
 	def validate_movement(*to_go)
-		if ((Direction.diagonal(to_go, @actual_position) or Direction.straight(to_go, @actual_position)) and Direction.one_step(to_go, @actual_position))
+		if  Direction.straight(to_go, @actual_position)
+			@actual_position = Asignation.new_position(to_go, @actual_position, @name, @color)
+		else
+			Asignation.invalid_movement
+		end
+	end
+end
+
+#Bishop class has a validate_movement method. It could be moved in any number of steps but only in straight direction
+class Bishop < Piece
+	def validate_movement(*to_go)
+		if Direction.diagonal(to_go, @actual_position)
+			@actual_position = Asignation.new_position(to_go, @actual_position, @name, @color)
+		else
+			Asignation.invalid_movement
+		end
+	end
+end
+
+#Horse class defines a validate_movement method. It moves describing and L, two steps in one axis and one in the other axis
+class Horse < Piece
+	def validate_movement(*to_go)
+		if Direction.horse(to_go, @actual_position)
+			@actual_position = Asignation.new_position(to_go, @actual_position, @name, @color)
+		else
+			Asignation.invalid_movement
+		end
+	end
+end
+
+#Pawn class
+class Pawn < Piece
+	def validate_movement(*to_go)
+		if Direction.pawn_normal(to_go, @actual_position)
 			@actual_position = Asignation.new_position(to_go, @actual_position, @name, @color)
 		else
 			Asignation.invalid_movement
@@ -63,9 +89,17 @@ end
 black_rook1 = Rook.new("Tower", "Black", 0, 0)
 black_bishop1 = Bishop.new("Bishop", "Black", 0, 0)
 black_queen= Queen.new("Reina del arroz con pollo","Black",0,0)
-black_rook1.validate_movement(1,1)
-black_rook1.validate_movement(0,1)
-black_bishop1.validate_movement(0,1)
-black_bishop1.validate_movement(1,1)
-black_queen.validate_movement(1,1)
-black_queen.validate_movement(1,0)
+black_horse1 = Horse.new("Cabashito", "Black", 0, 0)
+black_pawn1 = Pawn.new("The programmer", "Black", 0, 0)
+black_pawn1.validate_movement(0,0)
+black_pawn1.validate_movement(1,0)
+black_pawn1.validate_movement(1,3)
+black_pawn1.validate_movement(-1,1)
+
+
+
+
+
+
+
+
--- chess_mod.rb
+++ chess_mod.rb
@@ -1,4 +1,5 @@
 module Direction
+
 	def Direction.diagonal(to_go, actual_position)
 		(to_go[0]-actual_position[0]).abs == (to_go[1]-actual_position[1]).abs
 	end
@@ -7,9 +8,26 @@ module Direction
 		actual_position[0] == to_go[0] or actual_position[1] == to_go[1]
 	end
 
-	def one_step(to_go, actual_position)
+	def Direction.one_step(to_go, actual_position)
 		(to_go[0]-@actual_position[0]).abs <= 1
 	end
+
+	def Direction.horse(to_go, actual_position)
+		self.short_l(to_go, actual_position) or self.long_l(to_go, actual_position)
+	end
+
+	def Direction.pawn_normal(to_go, actual_position)
+		(actual_position[0]-to_go[0]).abs == 1 and actual_position[1] == to_go[1]
+	end
+
+	def self.short_l(to_go, actual_position)
+		(actual_position[0]-to_go[0]).abs == 2 and (actual_position[1]-to_go[1]).abs == 1
+	end
+
+	def self.long_l(to_go, actual_position)
+		(actual_position[0]-to_go[0]).abs == 1 and (actual_position[1]-to_go[1]).abs == 2
+	end
+
 end
 
 module Asignation
